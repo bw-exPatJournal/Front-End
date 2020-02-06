1346b820a883f03441dfaf6535f994d722b5714a
Anthony Johnson <wontons713@Anthonys-MacBook-Pro.local>
2020-02-06 16:04:32 -0700

    still want to style it a bit but its working and clean enough.

--- expat-journal/src/components/Register/Register.js
+++ expat-journal/src/components/Register/Register.js
@@ -2,92 +2,151 @@ import React from "react";
 import { withFormik, Form, Field } from "formik";
 import * as Yup from "yup";
 import axios from 'axios';
+import styled from 'styled-components';
+import background from '../../imgs/background.png'
+import logo from '../../imgs/logo.png'
+
+
+const BackgroundImage = styled.div`
+    width:100%;
+    height:100vh;
+    background-image: url(${background});
+    background-size: cover;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    padding: 5%;
+    box-sizing: border-box;
+    `;
+
+const CaptureLogo = styled.div`
+    text-align: center;
+    background-image: url(${logo});
+    background-size: contain;
+    background-repeat: no-repeat;
+    width:15%;
+    height: 10vh;
+    margin-bottom: 2%;
+    `;
+
+
+const FormContainer = styled.div`
+    background: #F4F5F7;
+    display: flex;
+    justify-content: center;
+    `;
+
+const TextContainer = styled.div`
+    background: #FFFFFF;
+    border: 0.3px solid #959595;
+    border-radius: 5px;
+    padding-top: 5%;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    width: 60%;
+    margin: 0 auto;
+    `;
+
+const BioContainer = styled.div`
+    background: #FFFFFF;
+    border: 0.3px solid #959595;
+    border-radius: 5px;
+    padding-top: 5%;
+    display: flex;
+    justify-content: flex-start;
+    height: 10vh;
+    flex-direction: column;
+    margin: 0 auto;
+    width: 60%;
+    `;
+
+
+const Button = styled.button`
+    width: 300px;
+    height: 35px;
+    background: #38A1DE;
+    border-radius: 5px;
+    color: white;
+    margin-top: 10%;
+    margin-bottom: 15%;
+    `;
+
+const P = styled.p`
+    font-size: 0.6rem;
+    `;
 
 function RegisterForm({ status, values, errors, touched }) {
-  console.log('Values:', values)
-  return (
-    <div className='FormContainer'>
-      <Form>
-        <div>
-          {touched.name && errors.name && <p>{errors.name}</p>}
-          <Field type='name' name='name' placeholder='Name' />
-        </div>
-        <div>
-          {touched.email && errors.email && <p>{errors.email}</p>}
-          <Field type='email' name='email' placeholder='Email' />
-        </div>
-        <div>
-          {touched.password && errors.password && <p>{errors.password}</p>}
-          <Field type='password' name='password' placeholder='Password' />
-        </div>
-        <div>
-          {touched.username && errors.username && <p>{errors.username}</p>}
-          <Field type='text' name='username' placeholder='Username' />
-        </div>
-        <div>
-          {touched.bio && errors.bio && <p>{errors.bio}</p>}
-          <Field type='text' name='bio' placeholder='Bio' />
-        </div>
-        <button type='submit'>Register!</button>
-      </Form>
-
-    </div>
-  );
+    console.log('Values:', values)
+    return (
+        <BackgroundImage id='BackgroundImage'>
+            <CaptureLogo></CaptureLogo>
+            
+                <FormContainer>
+                    <Form>
+                        <TextContainer>
+                            <Field type='name' name='name' placeholder='Name'/>
+                            {touched.name && errors.name && <P>{errors.name}</P>}
+                        </TextContainer>
+                        <TextContainer>
+                            <Field type='email' name='email' placeholder='Email' />
+                            {touched.email && errors.email && <P>{errors.email}</P>}
+                        </TextContainer>
+                        <TextContainer>
+                            <Field type='password' name='password' placeholder='Password' />
+                            {touched.password && errors.password && <P>{errors.password}</P>}
+                        </TextContainer>
+                        <TextContainer>
+                            <Field type='text' name='username' placeholder='Username' />
+                            {touched.username && errors.username && <P>{errors.username}</P>}
+                        </TextContainer>
+                        <BioContainer>
+                            <Field type='text' name='bio' placeholder='Bio' />
+                            {touched.bio && errors.bio && <P>{errors.bio}</P>}
+                        </BioContainer>
+                        <Button type='submit'>Register!</Button>
+                    </Form>
+                </FormContainer>
+           
+        </BackgroundImage>
+    );
 }
 
 const FormikRegisterForm = withFormik({
-  mapPropsToValues({ name, email, username, password, bio }) {
-    return {
-      name: name || '',
-      email: email || '',
-      username: username || '',
-      password: password || '',
-      bio: bio || ''
-    };
-  },
+    mapPropsToValues({ name, email, username, password, bio }) {
+        return {
+            name: name || '',
+            email: email || '',
+            username: username || '',
+            password: password || '',
+            bio: bio || ''
+        };
+    },
 
-  validationSchema: Yup.object().shape({
-    name: Yup.string().required('Please enter name'),
-    email: Yup.string().required('Email is required'),
-    username: Yup.string().required('Please enter username'),
-    password: Yup.string().min(6, 'Password must be 6 characters').required('Please enter password'),
-    bio: Yup.string().required('Please enter a bio')
-  }),
+    validationSchema: Yup.object().shape({
+        name: Yup.string().required('Please enter name'),
+        email: Yup.string().required('Email is required'),
+        username: Yup.string().required('Please enter username'),
+        password: Yup.string().min(6, 'Password must be 6 characters').required('Please enter password'),
+        bio: Yup.string().required('Please enter a bio')
+    }),
 
-<<<<<<< HEAD
-    handleSubmit(values, {setStatus}){
+    handleSubmit(values, { setStatus }) {
         console.log('values object:', values);
         axios
-        .post('https://expatjournalbackend.herokuapp.com/api/auth/register', values)
-        .then(res => {
-            console.log('values object:', values);
-            console.log('info from api', res);
-            
-        })
-        .catch(err => {
-            console.log('values object:', values) 
-            console.log(err.response)
-        }
-    );
-    }
-  })(RegisterForm);
-=======
-  handleSubmit(values, { setStatus }) {
-    console.log('values object:', values);
-    axios
-      .post('https://expatjournalbackend.herokuapp.com/api/auth/register', values)
-      .then(res => {
-        console.log('values object:', values);
-        console.log('info from api', res);
+            .post('https://expatjournalbackend.herokuapp.com/api/auth/register', values)
+            .then(res => {
+                console.log('values object:', values);
+                console.log('info from api', res);
 
-      })
-      .catch(err => {
-        console.log('values object:', values)
-        console.log(err.response)
-      }
-      );
-  }
+            })
+            .catch(err => {
+                console.log('values object:', values)
+                console.log(err.response)
+            }
+            );
+    }
 })(RegisterForm);
->>>>>>> 70e7a6e1dd44c4c809abf98ae16546856fd2545c
 
 export default FormikRegisterForm;
---
+++ expat-journal/src/components/Register/RegisterStyles.jsx
@@ -0,0 +1,45 @@
+// import React from "react";
+// import styled from 'styled-components';
+
+// const BackgroundImage=  styled.div`
+//     width:100%;
+//     height:100%;
+//     background-image: url(./imgs/background.png')
+//     `;
+
+// const CaptureTitle = styled.h1`
+//     font-family: Lobster;
+//     font-style: normal;
+//     font-weight: normal;
+//     font-size: 96px;
+//     line-height: 120px;
+//     text-align: center;
+//     color: #3C5955;
+//     `;
+
+// const FormContainer = styled.div`
+//     background: #F4F5F7;
+//     `;
+
+// const TextContainer = styled.div`
+//     background: #FFFFFF;
+//     border: 0.2px solid #959595;
+//     border-radius: 5px;
+//     `;
+
+// const BioContainer = styled.div`
+//     background: #FFFFFF;
+//     border: 0.2px solid #959595;
+//     border-radius: 5px;
+//     `;
+
+// const Button = styled.button`
+//     width: 300px;
+//     height: 35px;
+//     background: #38A1DE;
+//     border-radius: 5px;
+//     `;
+
+
+
+
