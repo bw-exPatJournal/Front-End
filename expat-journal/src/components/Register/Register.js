a539fd338ac5a6015a7cd62d1f40cdff43270a18
Anthony Johnson <wontons713@Anthonys-MacBook-Pro.local>
2020-02-06 16:53:38 -0700

    fixed up

--- expat-journal/src/components/Register/Register.js
+++ expat-journal/src/components/Register/Register.js
@@ -1,81 +1,8 @@
 import React from "react";
 import { withFormik, Form, Field } from "formik";
+import {BackgroundImage, CaptureLogo, FormContainer, TextContainer, BioContainer, Button, P} from './RegisterStyles'
 import * as Yup from "yup";
 import axios from 'axios';
-import styled from 'styled-components';
-import background from '../../imgs/background.png'
-import logo from '../../imgs/logo.png'
-
-
-const BackgroundImage = styled.div`
-    width:100%;
-    height:100vh;
-    background-image: url(${background});
-    background-size: cover;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    padding: 5%;
-    box-sizing: border-box;
-    `;
-
-const CaptureLogo = styled.div`
-    text-align: center;
-    background-image: url(${logo});
-    background-size: contain;
-    background-repeat: no-repeat;
-    width:15%;
-    height: 10vh;
-    margin-bottom: 2%;
-    `;
-
-
-const FormContainer = styled.div`
-    background: #F4F5F7;
-    display: flex;
-    justify-content: center;
-    `;
-
-const TextContainer = styled.div`
-    background: #FFFFFF;
-    border: 0.3px solid #959595;
-    border-radius: 5px;
-    padding-top: 5%;
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-    width: 60%;
-    margin: 0 auto;
-    `;
-
-const BioContainer = styled.div`
-    background: #FFFFFF;
-    border: 0.3px solid #959595;
-    border-radius: 5px;
-    padding-top: 5%;
-    display: flex;
-    justify-content: flex-start;
-    height: 10vh;
-    flex-direction: column;
-    margin: 0 auto;
-    width: 60%;
-    `;
-
-
-const Button = styled.button`
-    width: 300px;
-    height: 35px;
-    background: #38A1DE;
-    border-radius: 5px;
-    color: white;
-    margin-top: 10%;
-    margin-bottom: 15%;
-    `;
-
-const P = styled.p`
-    font-size: 0.6rem;
-    `;
 
 function RegisterForm({ status, values, errors, touched }) {
     console.log('Values:', values)
